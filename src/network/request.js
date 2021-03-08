a42810adb357c8a4e93b2dc31faa585b30d235e6
hongsheng666 <75429427+hongsheng666@users.noreply.github.com>
2021-03-08 15:13:13 +0800

    Update request.js

--- src/network/request.js
+++ src/network/request.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export function request(config) {
   const instance = axios.create({
-    baseURL: 'http://152.136.185.210:7878/api/m5',
+    baseURL: '找老师要',
     timeout:5000
   })
   // 响应拦截
